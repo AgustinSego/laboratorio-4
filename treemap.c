95c56f3fb59eb780c5ad0ce891dcc267217f2cd6
Your Name <you@example.com>
2026-04-30 20:31:34 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -124,7 +124,7 @@ Pair * nextTreeMap(TreeMap * tree) {
             if(aux2->pair->key > aux->pair->key || aux2->parent == NULL)break;
             aux2 = aux2->parent;
         }
-        //tree->current = aux2;
+        tree->current = aux2;
         
         return aux2->pair; 
     }
