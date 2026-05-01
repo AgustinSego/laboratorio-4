065cca3ea93d49edb324b309b0df8d4f07a5ddbb
Your Name <you@example.com>
2026-05-01 09:01:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -186,7 +186,7 @@ Pair * nextTreeMap(TreeMap * tree){
 
 Pair * upperBound(TreeMap * tree, void* key) {
     if(searchTreeMap(tree, key) == NULL){
-        return nextTreeMap(tree);
+        return NULL;
     }else{
         return searchTreeMap(tree, key);
     }
